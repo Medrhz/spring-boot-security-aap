78c93630b0de90450718cc1ae326ef011c84bea2
RHZIZA Mohamed <109476153+Medrhz@users.noreply.github.com>
2026-04-19 05:18:51 +0100

    create project and add dependencies

---
+++ src/test/java/com/mystore/secureauthapplication/SecureAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mystore.secureauthapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureAuthApplicationTests {
+
+   @Test
+   void contextLoads () {
+   }
+
+}
